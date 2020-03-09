fa64a3ad07b473eb0021744a48a5aa1cdb2654dd
buaa g306 <you@example.com>
2020-03-09 17:19:53 +0800

    Initial lab1

--- csc/code/modify.sh
+++
@@ -1,5 +0,0 @@
-file=$1
-s=$2
-t=$3
-
-sed -i "s/$s/$t/g" $file
--- dst/sh_test/hello_os.sh
+++
@@ -1,8 +0,0 @@
-in=$1
-out=$2
-
-sed -n '8,1p' $in > $out
-sed -n '32,1p' $in >> $out
-sed -n '128,1p' $in >> $out
-sed -n '512,1p' $in >> $out
-sed -n '1024,1p' $in >> $out
