7299255b58ba7633d5b81b381cb32a8499c83693
dragonylee <dragonylee@163.com>
2020-04-14 20:15:59 +0000

    all

--- lib/kclock.c
+++ lib/kclock.c
@@ -3,16 +3,13 @@
 /* The Run Time Clock and other NVRAM access functions that go with it. */
 /* The run time clock is hard-wired to IRQ4. */
 
-
 #include <kclock.h>
 
 extern void set_timer();
 
 /*** exercise 3.13 ***/
-void
-kclock_init(void)
+void kclock_init(void)
 {
-	// hint: use set_timer()
-	
+    // hint: use set_timer()
+    set_timer();
 }
-
--- lib/sched.c
+++ lib/sched.c
@@ -16,12 +16,13 @@ void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
-    
+    struct Env *e = LIST_FIRST(&env_sched_list[point]);
+
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.
      *  2. if (env_sched_list[point] is empty), point = 1 - point;
-     *     then search through `env_sched_list[point]` for a runnable env `e`, 
+     *     then search through `env_sched_list[point]` for a runnable env `e`,
      *     and set count = e->env_pri
      *  3. count--
      *  4. env_run()
@@ -29,4 +30,30 @@ void sched_yield(void)
      *  functions or macros below may be used (not all):
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
+    if (count)
+    {
+        count--;
+        env_run(e);
+    }
+    if (!count)
+    {
+        LIST_REMOVE(e, env_sched_link);
+        LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+    }
+    if (LIST_EMPTY(&env_sched_list[point]))
+    {
+        point = 1 - point;
+    }
+    e = LIST_FIRST(&env_sched_list[point]);
+    while (e->env_status != ENV_RUNNABLE)
+    {
+        e = LIST_NEXT(e, env_sched_link);
+        if (e == NULL)
+        {
+            point = 1 - point;
+            e = LIST_FIRST(&env_sched_list[point]);
+        }
+    }
+    count = e->env_pri - 1;
+    env_run(e);
 }
