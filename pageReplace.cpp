c916ebfcf6bed8bfb68109a432eed04cea599a4f
dragonylee <dragonylee@163.com>
2020-04-23 10:33:22 +0000

    change to global

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,12 +1,14 @@
+#pragma GCC optimize(2)
 #include "pageReplace.h"
 #define MAX_PHY_PAGE 64
+
+int clock = 0;                   // 时间戳
+char where[1050000];             // 记录某个虚拟页面所在的物理页框(+1)，与physic_memery互逆
+int last_time[MAX_PHY_PAGE + 5]; // 记录每个物理页框最近一次访问的时间戳
 
 // LRU算法
 void pageReplace(long *physic_memery, long nwAdd)
 {
-    static int clock = 0;                   // 时间戳
-    static char where[1050000];             // 记录某个虚拟页面所在的物理页框(+1)，与physic_memery互逆
-    static int last_time[MAX_PHY_PAGE + 5]; // 记录每个物理页框最近一次访问的时间戳
     long page = nwAdd >> 12;
 
     if (clock < MAX_PHY_PAGE) // 表示物理页框还有空余
@@ -22,7 +24,7 @@ void pageReplace(long *physic_memery, long nwAdd)
         return;
     }
 
-    clock++;
+    ++clock;
 
     if (where[page]) // page在物理页框中，更新时间戳
     {
@@ -31,7 +33,7 @@ void pageReplace(long *physic_memery, long nwAdd)
     else // 寻找最小的时间戳（最久未使用）的index，替换页面
     {
         int idx = 0, temp = 1 << 30;
-        for (int i = 0; i < MAX_PHY_PAGE; i++)
+        for (register int i = 0; i < MAX_PHY_PAGE; ++i)
             if (last_time[i] < temp)
                 temp = last_time[i], idx = i;
 
